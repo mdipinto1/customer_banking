0626130b4a1dd5d319864656ecec6a3af572ba5b
Matt DiPinto <dipinto.matt@gmail.com>
2023-11-10 15:35:20 -0700

    added error handling

--- customer_banking.py
+++ customer_banking.py
@@ -8,18 +8,23 @@ def main():
     and the length of months to determine the interest gained.
     It displays the interest earned on the savings and CD accounts and updates the balances.
     """
+    
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     savings_balance = float(input("\nPlease enter the starting balance of your savings account. "))
     savings_interest = float(input('Please enter the interest rate of your savings account. '))
     savings_maturity = int(input('Please enter the number of months your account will accrue interest over. '))
 
     # Call the create_savings_account function and pass the variables from the user.
-    updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
-
+    if all(var > 0 for var in (savings_balance, savings_interest, savings_maturity)):
+        updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
+    
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
-    print(f"\nYour final balance after {savings_maturity} months will be ${updated_savings_balance:,.2f}.")
-    print(f"Your earned interest on your original balance of ${savings_balance:,.2f} will be ${interest_earned:,.2f}.")
+        print(f"\nYour final balance after {savings_maturity} months will be ${updated_savings_balance:,.2f}.")
+        print(f"Your earned interest on your original balance of ${savings_balance:,.2f} will be ${interest_earned:,.2f}.")
 
+    #else statement for out of range variable catching
+    else:
+        print("One of the Savings Account variables entered was less than zero and therefore invalid")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     cd_balance = float(input("\nPlease enter the starting balance of your CD account. "))
@@ -27,11 +32,15 @@ def main():
     cd_maturity = int(input('Please enter the number of months your account will accrue interest over. '))
 
     # Call the create_cd_account function and pass the variables from the user.
-    updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
+    if all(var > 0 for var in (cd_balance, cd_interest, cd_maturity)):
+        updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
-    # Print out the interest earned and updated CD account balance with interest earned for the given months.
-    print(f"\nYour final balance after {cd_maturity} months will be ${updated_cd_balance:,.2f}.")
-    print(f"Your earned interest on your original balance of ${cd_balance:,.2f} will be ${interest_earned:,.2f}.")
+        # Print out the interest earned and updated CD account balance with interest earned for the given months.
+        print(f"\nYour final balance after {cd_maturity} months will be ${updated_cd_balance:,.2f}.")
+        print(f"Your earned interest on your original balance of ${cd_balance:,.2f} will be ${interest_earned:,.2f}.")
+
+    else:
+        print("One of the CD variables entered was less than zero and therefore invalid")
 
 if __name__ == "__main__":
     # Call the main function.
